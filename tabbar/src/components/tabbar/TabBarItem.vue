b742a12ccee95558980935e7ae09251c1e109b3d
Lawrence-Buddha <2493451720@qq.com>
2021-02-12 10:39:28 +0800

    tabbar-second-committed

---
+++ tabbar/src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <!--  所有的item都展示同一个图片，同一个文字-->
+  <div class="tab-bar-item">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="item-text"></slot>
+    </div>
+
+    <!--      <img src="../../assets/img/tabbar/home.svg" alt="">-->
+    <!--      <div>首页</div>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  data () {
+    return {
+      isActive: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.tab-bar-item {
+  /*均等分*/
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+
+.tab-bar-item img {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  /*图片基线对齐而不是中线*/
+  vertical-align: middle;
+  margin-bottom: 2px;
+}
+.active{
+  color: red;
+}
+</style>
